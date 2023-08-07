f44d0378c2e3adf8bf8150e31eda5c10df4a2a5a
Dhinesh Kumar <52561493+anbudina90@users.noreply.github.com>
2023-08-07 20:45:54 +0530

    Add files via upload

---
+++ BodmasRule.java
@@ -0,0 +1,10 @@
+
+public class BodmasRule {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(10*10/5+3-1*4/2);  
+
+	}
+
+}
---
+++ LogicalBitwise.java
@@ -0,0 +1,13 @@
+
+public class LogicalBitwise {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;  
+		int b=5;  
+		int c=20;  
+		System.out.println(a<b&&a<c);//false && true = false  
+		System.out.println(a<b&a<c);//false & true = false  
+	}
+
+}
